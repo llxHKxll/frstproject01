cc328013c06e6390ea36fe2b83a0b4d3dc8e0e25
🦋⃟ᬊ 𝗛ᴋ <161941135+llxHKxll@users.noreply.github.com>
2024-12-09 15:41:40 +0530

    Update guess.py

--- Bot/guess.py
+++ Bot/guess.py
@@ -1,8 +1,19 @@
 import random
 import time
+from pyrogram import Client
 from threading import Timer
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from Bot.app import app
+
+API_ID = "21989020"
+API_HASH = "3959305ae244126404702aa5068ba15c"
+BOT_TOKEN = "8141351816:AAG1_YB0l88X0SLAHnos9iODdZuCdNEfuFo"
+
+app = Client(
+  name="Kaisen Ranking Bot",
+  api_id=API_ID,
+  api_hash=API_HASH,
+  bot_token=BOT_TOKEN
+)
 
 # Game data storage
 current_game = {}  # Stores game data for each chat
