888b929abb571d7b37776e51d550ec9de2082994
🦋⃟ᬊ 𝗛ᴋ <161941135+llxHKxll@users.noreply.github.com>
2024-12-09 16:19:38 +0530

    Update main.py

--- Bot/main.py
+++ Bot/main.py
@@ -10,6 +10,7 @@ from Bot.daily import claim_daily_reward
 from Bot.leaderboard import update_leaderboard_message, leaderboard_modes, prepare_leaderboard_message  # Import leaderboard functions
 from Bot.poll import start_poll, handle_vote, show_poll_results, BOT_ADMIN_ID
 from Bot.shop import get_shop_page, handle_purchase
+from Bot.guess import start_game, join_game, force_start_game, cancel_game, process_guess  # Import all guess-related functions
 from database.db_manager import create_db, add_user, ensure_user_exists, get_user, update_points, update_level, update_health, connect_db
 
 API_ID = "21989020"
